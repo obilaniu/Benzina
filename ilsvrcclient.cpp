4dc3a860c859476143b1ca9d9c916bd7b685569e
Olexa Bilaniuk <obilaniu@gmail.com>
2017-02-28 23:40:26 -0500

    Added connection handling and new quit request.
    Request-reply now works.

--- ilsvrcclient.cpp
+++ ilsvrcclient.cpp
@@ -21,19 +21,27 @@
 int   main(int argc, char* argv[]){
 	void* ctx       = zmq_ctx_new();
 	void* sock      = zmq_socket(ctx, ZMQ_REQ);
-	char* msg;
-	int   N;
+	char  msg[8];
+	int   numBytes, N, ret;
 	
 	zmq_connect(sock, "tcp://127.0.0.1:5555");
 	
-	msg = (char*)"Hello World!";
-	N   = strlen(msg);
-	int   numBytes  = zmq_send(sock, msg, N+1, 0);
+	memset(msg, 0, sizeof(msg));
+	N        = sizeof(msg);
+	numBytes = zmq_send(sock, msg, N, 0);
 	if(numBytes<0){
 		printf("Error! Couldn't send message.\n");
 		return 1;
 	}else{
 		printf("Sent %d bytes.\n", numBytes);
+	}
+	
+	numBytes = zmq_recv(sock, &ret, sizeof(ret), 0);
+	if(numBytes<0){
+		printf("Error! Didn't receive valid reply message!\n");
+		return 1;
+	}else{
+		printf("Request returned error code %d.\n", ret);
 	}
 	
 	zmq_close(sock);
--- ilsvrcserver.cpp
+++ ilsvrcserver.cpp
@@ -7,22 +7,25 @@
  * 
  * ilsvrcserver [args] path/to/dataset.hdf5 <port>
  * 
+ *   -n       madvise() for normal access.
  *   -r       madvise() for random access.
  *   -s       madvise() for sequential access.
- *   -l       mlock()   the dataset.
+ *   -l       mlock()   the dataset into memory.
  *   <port>   Localhost port number to bind to. Default 5555.
  * 
  * IMPLEMENTATION:
  * 
- *   The server is stateless, and accepts only a single ZeroMQ request packet,
- *   whose payload is as follows:
+ *   The server is stateless, and accepts three ZeroMQ request packets,
+ *   whose payload are as follows:
  *
  * PACKET FORMAT:
  *   Version:      00 00 00 00
  *   Request#:     xx xx xx xx
  *   {...}
  * 
- * Request#: 00 00 00 00 (Sequential Batch)
+ * Request#: 00 00 00 00 (Exit)
+ * 
+ * Request#: 01 00 00 00 (Sequential Batch)
  *   Batch Size:   xx xx xx xx xx xx xx xx
  *   Start Index:  xx xx xx xx xx xx xx xx
  *   X Off:        xx xx xx xx xx xx xx xx
@@ -32,7 +35,7 @@
  *   X Path:       {xx}*X Path Len 00
  *   Y Path:       {xx}*Y Path Len 00
  * 
- * Request#: 01 00 00 00 (Random Batch)
+ * Request#: 02 00 00 00 (Random Batch)
  *   Batch Size:   xx xx xx xx xx xx xx xx
  *   Indexes:      {xx xx xx xx xx xx xx xx}*Batch Size
  *   X Off:        xx xx xx xx xx xx xx xx
@@ -75,21 +78,35 @@ struct ILSVRC{
 struct ILSVRC_CONN;
 typedef struct ILSVRC_CONN ILSVRC_CONN;
 struct ILSVRC_CONN{
-	zmq_msg_t zmqMsgId;
-	zmq_msg_t zmqMsgData;
-	zmq_msg_t zmqMsgDis;
+	zmq_msg_t   zmqMsgId;
+	zmq_msg_t   zmqMsgDel;
+	zmq_msg_t   zmqMsgData;
+	zmq_msg_t   zmqMsgDis;
+	const char* msg;
+	size_t      msgLen;
+	uint32_t    ver;
+	uint32_t    req;
 };
 
 
 /* Static Function Prototypes */
-static int   ilsvrcHasRequestedExit   (ILSVRC* s);
-static void  ilsvrcRequestExitWithCode(ILSVRC* s, int ret);
-static int   ilsvrcParseArgs          (ILSVRC* s, int argc, char** argv);
-static int   ilsvrcPrintHelp          (ILSVRC* s);
-static int   ilsvrcZmqSetup           (ILSVRC* s);
-static int   ilsvrcEventLoop          (ILSVRC* s);
-static void  ilsvrcHandleEvent        (ILSVRC* s);
-static int   ilsvrcTeardown           (ILSVRC* s, int ret);
+static int   ilsvrcHasRequestedExit          (ILSVRC* s);
+static void  ilsvrcRequestExitWithCode       (ILSVRC* s, int ret);
+static int   ilsvrcParseArgs                 (ILSVRC* s, int argc, char** argv);
+static int   ilsvrcPrintHelp                 (ILSVRC* s);
+static int   ilsvrcZmqSetup                  (ILSVRC* s);
+static int   ilsvrcEventLoop                 (ILSVRC* s);
+static int   ilsvrcHandleEvent               (ILSVRC* s);
+static int   ilsvrcTeardown                  (ILSVRC* s, int ret);
+static int   ilsvrcConnInit                  (ILSVRC* s, ILSVRC_CONN* c);
+static int   ilsvrcConnAccept                (ILSVRC* s, ILSVRC_CONN* c);
+static int   ilsvrcConnProcess               (ILSVRC* s, ILSVRC_CONN* c);
+static int   ilsvrcConnReply                 (ILSVRC* s, ILSVRC_CONN* c, int ret);
+static int   ilsvrcConnDiscard               (ILSVRC* s, ILSVRC_CONN* c);
+static int   ilsvrcConnCleanup               (ILSVRC* s, ILSVRC_CONN* c);
+static int   ilsvrcConnRequestExit           (ILSVRC* s, ILSVRC_CONN* c);
+static int   ilsvrcConnRequestSequentialBatch(ILSVRC* s, ILSVRC_CONN* c);
+static int   ilsvrcConnRequestRandomBatch    (ILSVRC* s, ILSVRC_CONN* c);
 
 
 
@@ -211,57 +228,10 @@ static int   ilsvrcEventLoop(ILSVRC* s){
  * Handle one event.
  */
 
-static void  ilsvrcHandleEvent     (ILSVRC* s){
+static int   ilsvrcHandleEvent     (ILSVRC* s){
 	ILSVRC_CONN c_STACK, *c = &c_STACK;
-	int         hasMore = 1, nbId, nbMsg, nbDis;
-	size_t      optlen = sizeof(hasMore);
-	zmq_msg_init(&c->zmqMsgId);
-	zmq_msg_init(&c->zmqMsgData);
-	zmq_msg_init(&c->zmqMsgDis);
 	
-	/**
-	 * A valid ZMQ message will be in three parts:
-	 *   - Identity
-	 *   - Empty Delimiter
-	 *   - Payload
-	 */
-	
-	nbDis = zmq_msg_recv(&c->zmqMsgDis, s->zmqSock, 0);
-	if(nbDis  <= 0){
-		printf("Error (ID receive)! %s\n", zmq_strerror(zmq_errno()));
-		goto discard;
-	}else{
-		zmq_msg_move(&c->zmqMsgId,   &c->zmqMsgDis);
-		nbId   = nbDis;
-		(void)nbId;
-	}
-	nbDis = zmq_msg_recv(&c->zmqMsgDis, s->zmqSock, 0);
-	if(nbDis != 0){
-		printf("Error (delimiter receive)! %s\n", zmq_strerror(zmq_errno()));
-		goto discard;
-	}
-	nbDis = zmq_msg_recv(&c->zmqMsgDis, s->zmqSock, 0);
-	if(nbDis<0){
-		printf("Error (payload receive)! %s\n", zmq_strerror(zmq_errno()));
-		goto discard;
-	}else{
-		zmq_msg_move(&c->zmqMsgData, &c->zmqMsgDis);
-		nbMsg  = nbDis;
-		printf("%d of payload.\n", nbMsg);
-	}
-	printf("'%s'\n", (const char*)zmq_msg_data(&c->zmqMsgData));
-	fflush(stdout);
-	
-	/* Discard. */
-	discard:
-	while(zmq_getsockopt(s->zmqSock, ZMQ_RCVMORE, &hasMore, &optlen) == 0 && hasMore){
-		nbDis = zmq_msg_recv(&c->zmqMsgDis, s->zmqSock, 0);
-	}
-	
-	
-	zmq_msg_close(&c->zmqMsgId);
-	zmq_msg_close(&c->zmqMsgData);
-	zmq_msg_close(&c->zmqMsgDis);
+	return ilsvrcConnInit(s, c);
 }
 
 /**
@@ -277,6 +247,158 @@ static int   ilsvrcTeardown(ILSVRC* s, int ret){
 	return s->exitCode;
 }
 
+/**
+ * Initialize connection.
+ */
+
+static int   ilsvrcConnInit(ILSVRC* s, ILSVRC_CONN* c){
+	memset(c, 0, sizeof(*c));
+	zmq_msg_init(&c->zmqMsgId);
+	zmq_msg_init(&c->zmqMsgDel);
+	zmq_msg_init(&c->zmqMsgData);
+	zmq_msg_init(&c->zmqMsgDis);
+	
+	return ilsvrcConnAccept(s, c);
+}
+
+/**
+ * Accept a connection.
+ */
+
+static int   ilsvrcConnAccept(ILSVRC* s, ILSVRC_CONN* c){
+	/**
+	 * A valid ZMQ message will be in three parts:
+	 *   - Identity
+	 *   - Empty Delimiter
+	 *   - Payload
+	 */
+	
+	if(zmq_msg_recv(&c->zmqMsgDis, s->zmqSock, 0)  <= 0){
+		fprintf(stderr, "Error (ID receive)! %s\n",        zmq_strerror(zmq_errno()));
+		return ilsvrcConnDiscard(s, c);
+	}
+	zmq_msg_move(&c->zmqMsgId,   &c->zmqMsgDis);
+	if(zmq_msg_recv(&c->zmqMsgDis, s->zmqSock, 0)  != 0){
+		fprintf(stderr, "Error (delimiter receive)! %s\n", zmq_strerror(zmq_errno()));
+		return ilsvrcConnDiscard(s, c);
+	}
+	zmq_msg_move(&c->zmqMsgDel,  &c->zmqMsgDis);
+	if(zmq_msg_recv(&c->zmqMsgDis, s->zmqSock, 0)   < 0){
+		fprintf(stderr, "Error (payload receive)! %s\n",   zmq_strerror(zmq_errno()));
+		return ilsvrcConnDiscard(s, c);
+	}
+	zmq_msg_move(&c->zmqMsgData, &c->zmqMsgDis);
+	c->msg    = (const char*)zmq_msg_data(&c->zmqMsgData);
+	c->msgLen = (size_t)     zmq_msg_size(&c->zmqMsgData);
+	
+	return ilsvrcConnProcess(s, c);
+}
+
+/**
+ * Process request.
+ */
+
+static int   ilsvrcConnProcess(ILSVRC* s, ILSVRC_CONN* c){
+	if(c->msgLen < 8){
+		return ilsvrcConnReply(s, c, 1);
+	}
+	
+	memcpy(&c->ver, c->msg+0, sizeof(c->ver));
+	memcpy(&c->req, c->msg+4, sizeof(c->req));
+	
+	if(c->ver > 0){
+		return ilsvrcConnReply(s, c, 2);
+	}
+	
+	switch(c->req){
+		case 0:  return ilsvrcConnRequestExit           (s, c);
+		case 1:  return ilsvrcConnRequestSequentialBatch(s, c);
+		case 2:  return ilsvrcConnRequestRandomBatch    (s, c);
+		default: return ilsvrcConnReply                 (s, c, 3);
+	}
+}
+
+/**
+ * Send reply packet back. This packet will contain 4 bytes only, a return
+ * code of sorts.
+ */
+
+static int   ilsvrcConnReply(ILSVRC* s, ILSVRC_CONN* c, int ret){
+	/**
+	 * A valid ZMQ message will be in three parts:
+	 *   - Identity
+	 *   - Empty Delimiter
+	 *   - Payload
+	 */
+	
+	if(zmq_msg_send(&c->zmqMsgId,   s->zmqSock, ZMQ_SNDMORE)  <= 0){
+		fprintf(stderr, "Error (ID send)! %s\n",        zmq_strerror(zmq_errno()));
+		return ilsvrcConnDiscard(s, c);
+	}
+	if(zmq_msg_send(&c->zmqMsgDel,  s->zmqSock, ZMQ_SNDMORE)  != 0){
+		fprintf(stderr, "Error (delimiter send)! %s\n", zmq_strerror(zmq_errno()));
+		return ilsvrcConnDiscard(s, c);
+	}
+	if(zmq_send    (s->zmqSock, &ret, sizeof(ret),        0)  != sizeof(ret)){
+		fprintf(stderr, "Error (payload send)! %s\n",   zmq_strerror(zmq_errno()));
+		return ilsvrcConnDiscard(s, c);
+	}
+	
+	return ilsvrcConnDiscard(s, c);
+}
+
+/**
+ * Discard remaining packets in the message.
+ */
+
+static int   ilsvrcConnDiscard(ILSVRC* s, ILSVRC_CONN* c){
+	int    hasMore = 0;
+	size_t optlen  = sizeof(hasMore);
+	
+	while(zmq_getsockopt(s->zmqSock, ZMQ_RCVMORE, &hasMore, &optlen) == 0 &&
+	      hasMore                                                         &&
+	      zmq_msg_recv(&c->zmqMsgDis, s->zmqSock, 0)                 >= 0){}
+	
+	return ilsvrcConnCleanup(s, c);
+}
+
+/**
+ * Clean up connection.
+ */
+
+static int   ilsvrcConnCleanup(ILSVRC* s, ILSVRC_CONN* c){
+	zmq_msg_close(&c->zmqMsgId);
+	zmq_msg_close(&c->zmqMsgDel);
+	zmq_msg_close(&c->zmqMsgData);
+	zmq_msg_close(&c->zmqMsgDis);
+	
+	return 0;
+}
+
+/**
+ * Message: Exit
+ */
+
+static int   ilsvrcConnRequestExit           (ILSVRC* s, ILSVRC_CONN* c){
+	ilsvrcRequestExitWithCode(s, 0);
+	return ilsvrcConnReply(s, c, 0);
+}
+
+/**
+ * Message: Sequential Batch
+ */
+
+static int   ilsvrcConnRequestSequentialBatch(ILSVRC* s, ILSVRC_CONN* c){
+	return ilsvrcConnReply(s, c, 0);
+}
+
+/**
+ * Message: Random Batch
+ */
+
+static int   ilsvrcConnRequestRandomBatch    (ILSVRC* s, ILSVRC_CONN* c){
+	return ilsvrcConnReply(s, c, 0);
+}
 
 /**
  * Main
