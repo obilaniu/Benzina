904436d4d2eb6f8c96d468f374f23d5c1ea5bcef
Olexa Bilaniuk <obilaniu@gmail.com>
2018-07-19 07:23:56 -0400

    Release 0.0.0
    Initial Release.

--- src/benzina/__init__.py
+++ src/benzina/__init__.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
-from . import native
+#
+# We early-import __version__ so that all submodules can see this information
+# if needed.
+#
+# Other packages follow.
+#
+from .version import __version__
+from .        import native, version
--- src/benzina/conversion/runner.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-export HOME=`getent passwd $USER | cut -d':' -f6`
-source ~/.bashrc
-. activate znymky
-echo "$USER running on $HOSTNAME: $@"
-exec $@ 2>/dev/null
