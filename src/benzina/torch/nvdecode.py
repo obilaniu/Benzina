0e7c3c540fde18b73d23b5603dde3cb61bb06120
Olexa Bilaniuk <obilaniu@gmail.com>
2018-08-08 01:27:25 -0400

    Convert Python code to use context managers.

--- src/benzina/torch/nvdecode.py
+++ src/benzina/torch/nvdecode.py
@@ -2,7 +2,7 @@
 import benzina.native
 import gc
 import numpy                           as np
-import os
+import os, sys
 import torch
 import torch.utils.data
 
@@ -143,9 +143,9 @@ class NvdecodeDataLoaderIter:
 			raise self.stop_iteration
 		try:
 			if self.core_needs_init():
-				self.pull_first_batch()
+				self.pull_first_indices()
 				self.init_core()
-				self.push_first_batch()
+				self.push_first_indices()
 				with suppress(StopIteration):
 					self.fill_core()
 			else:
@@ -160,8 +160,8 @@ class NvdecodeDataLoaderIter:
 	def core_needs_init(self):
 		return self.core is None
 	
-	def pull_first_batch(self):
-		self.first_batch = next(self.batch_iter)
+	def pull_first_indices(self):
+		self.first_indices = next(self.batch_iter)
 		
 	def init_core(self):
 		"""
@@ -177,7 +177,7 @@ class NvdecodeDataLoaderIter:
 		"""
 		
 		self.garbage_collect()
-		self.check_or_set_batch_size(self.first_batch)
+		self.check_or_set_batch_size(self.first_indices)
 		self.multibuffer = torch.zeros([self.multibuffering,
 		                                self.batch_size,
 		                                3,
@@ -196,31 +196,29 @@ class NvdecodeDataLoaderIter:
 		                       self.shape[1],
 		                   )
 	
-	def push_first_batch(self):
-		self.push(self.__dict__.pop("first_batch"))
+	def push_first_indices(self):
+		self.push(self.__dict__.pop("first_indices"))
 	
 	def fill_core(self):
 		while self.core.pushes < self.core.multibuffering:
 			self.fill_one_batch()
 	
-	def push(self, batch):
-		self.check_or_set_batch_size(batch)
-		buffer = self.multibuffer[self.core.pushes % self.core.multibuffering, :len(batch)]
+	def push(self, indices):
+		self.check_or_set_batch_size(indices)
+		buffer  = self.multibuffer[self.core.pushes % self.core.multibuffering][:len(indices)]
+		indices = [int(i)                    for i in indices]
+		ptrs    = [int(buffer[n].data_ptr()) for n in range(len(indices))]
+		auxd    = [self.dataset[i]           for i in indices]
+		token   = (buffer, *self.collate_fn(auxd))
 		
-		aux = []
-		self.core.defineBatch()
-		for s,b in zip(batch, buffer):
-			s = int(s)
-			aux.append(self.dataset[s])
-			self.core.defineSample     (s, int(b.data_ptr()))
-			self.core.setHomography    (*self.warp_transform (self, s))
-			self.core.selectColorMatrix(*self.color_transform(self, s))
-			self.core.setBias          (*self.bias_transform (self, s))
-			self.core.setScale         (*self.scale_transform(self, s))
-			self.core.setOOBColor      (*self.oob_transform  (self, s))
-			self.core.submitSample()
-		aux = self.collate_fn(aux)
-		self.core.submitBatch((buffer, *aux))
+		with self.core.batch(token) as batch:
+			for i,ptr in zip(indices, ptrs):
+				with batch.sample(i, ptr):
+					self.core.setHomography    (*self.warp_transform (self, i))
+					self.core.selectColorMatrix(*self.color_transform(self, i))
+					self.core.setBias          (*self.bias_transform (self, i))
+					self.core.setScale         (*self.scale_transform(self, i))
+					self.core.setOOBColor      (*self.oob_transform  (self, i))
 	
 	def pull(self):
 		if self.core.pulls >= self.core.pushes:
@@ -230,8 +228,8 @@ class NvdecodeDataLoaderIter:
 	def fill_one_batch(self):
 		self.push(next(self.batch_iter))
 	
-	def check_or_set_batch_size(self, iter_batch):
-		iter_batch_size = len(iter_batch)
+	def check_or_set_batch_size(self, indices):
+		iter_batch_size = len(indices)
 		
 		if   self.batch_size is None:
 			self.batch_size = iter_batch_size
