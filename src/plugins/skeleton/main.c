55b2106c6e57daa639aa806778ffc92dd70c0b5f
Olexa Bilaniuk <obilaniu@gmail.com>
2018-11-28 10:34:24 -0500

    More refactoring of build system.
    - Make heavy use of declare_dependency() to remove much flag & include-
    directory code.
    - Add tool and plugin templates.

---
+++ src/plugins/skeleton/main.c
@@ -0,0 +1,34 @@
+/* Includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "benzina/benzina.h"
+
+
+
+/* Data Structure Definitions */
+
+
+
+/* Static Function Prototypes */
+
+
+
+/* Static Function Definitions */
+
+
+
+/* Public Function Definitions */
+
+/**
+ * @brief foo
+ * @param [in]  arg  Some argument
+ * @return 
+ */
+
+BENZINA_PLUGIN_PUBLIC void* benzina_foo(void* arg){
+    (void)arg;
+    printf("Hello, plugin world!\n");
+    return NULL;
+}
+
---
+++ src/tools/skeleton/main.c
@@ -0,0 +1,31 @@
+/* Includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+/* Data Structure Definitions */
+
+
+
+/* Static Function Prototypes */
+
+
+
+/* Static Function Definitions */
+
+
+
+
+/**
+ * Main
+ */
+
+int   main(int argc, char* argv[]){
+    (void)argc;
+    (void)argv;
+    printf("Hello, tool world!\n");
+    return 0;
+}
+
