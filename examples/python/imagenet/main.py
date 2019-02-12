c851833fa9736bd162501ed84ee852e0c2d26835
Satya Ortiz-Gagne <s.ortizgagne@gmail.com>
2019-02-12 11:33:02 -0500

    Fix number of validation and test in ImageNet 2012 dataset

--- examples/python/imagenet/main.py
+++ examples/python/imagenet/main.py
@@ -139,32 +139,22 @@ def main_worker(gpu, ngpus_per_node, args):
     dataset = bz.ImageNet(args.data)
 
     indices = list(range(len(dataset)))
-    n_valid = int(len(dataset) * 1 / 5)
-    train_sampler = torch.utils.data.SubsetRandomSampler(indices[:-n_valid])
-    valid_sampler = torch.utils.data.SubsetRandomSampler(indices[-n_valid:])
+    n_valid = 50000
+    n_test = 100000
+    n_train = len(dataset) - n_valid - n_test
+    train_sampler = torch.utils.data.SubsetRandomSampler(indices[:n_train])
+    valid_sampler = torch.utils.data.SubsetRandomSampler(indices[n_train:-n_test])
 
     # Dataloaders
     bias = ops.ConstantBiasTransform(bias=(123.675, 116.28 , 103.53))
     std = ops.ConstantNormTransform(norm=(58.395, 57.12 , 57.375))
 
-    train_loader = bz.DataLoader(
-        dataset,
-        batch_size=args.batch_size,
-        sampler=train_sampler,
-        seed=args.seed,
-        shape=(224,224),
-        bias_transform=bias,
-        norm_transform=std,
-        warp_transform=ops.SimilarityTransform(flip_h=0.5))
-    val_loader = bz.DataLoader(
-        dataset,
-        batch_size=args.batch_size,
-        sampler=valid_sampler,
-        seed=args.seed,
-        shape=(224,224),
-        bias_transform=bias,
-        norm_transform=std,
-        warp_transform=ops.SimilarityTransform())
+    train_loader = bz.DataLoader(dataset, batch_size=args.batch_size,
+        sampler=train_sampler, seed=args.seed, shape=(224,224), bias_transform=bias,
+        norm_transform=std, warp_transform=ops.SimilarityTransform(flip_h=0.5))
+    val_loader = bz.DataLoader(dataset, batch_size=args.batch_size,
+        sampler=valid_sampler, seed=args.seed, shape=(224,224), bias_transform=bias,
+        norm_transform=std, warp_transform=ops.SimilarityTransform())
     ### Benzina - end ###
 
     if args.evaluate:
