a3991554f97b0638633e231d390f6bab57ac697c
Olexa Bilaniuk <obilaniu@gmail.com>
2019-11-28 05:21:36 -0500

    Rename typedefs and enums under iso/ to have the BENZ_ISO_* prefix.
    No functionality change.

--- src/libbenzina/iso.c
+++ src/libbenzina/iso.c
@@ -17,7 +17,6 @@
  *   - We want to dlsym() these symbols.
  */
 
-
 BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint8_t     benz_iso_as_u8                   (const void* p);
 BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint16_t    benz_iso_as_u16                  (const void* p);
 BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint32_t    benz_iso_as_u32                  (const void* p);
@@ -35,21 +34,21 @@ BENZINA_PUBLIC BENZINA_EXTERN_INLINE void*       benz_iso_put_ascii             
 BENZINA_PUBLIC BENZINA_EXTERN_INLINE void*       benz_iso_put_asciz               (void*       p, const char* str);
 BENZINA_PUBLIC BENZINA_EXTERN_INLINE void*       benz_iso_put_bytes               (void*       p, const void* buf, uint64_t len);
 BENZINA_PUBLIC BENZINA_EXTERN_INLINE int         benz_iso_is_type_equal           (const void* p, const void* type);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint64_t    benz_iso_box_get_size            (BENZ_BOX    p);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint64_t    benz_iso_box_get_size_bounded    (BENZ_BOX    p, uint64_t max_size);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE const char* benz_iso_box_get_type_str        (BENZ_BOX    p);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint32_t    benz_iso_box_get_type            (BENZ_BOX    p);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE int         benz_iso_box_is_type             (BENZ_BOX    p, const void* type);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE const char* benz_iso_box_get_exttype         (BENZ_BOX    p);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE const void* benz_iso_box_get_payload         (BENZ_BOX    p);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint64_t    benz_iso_fullbox_get_size        (BENZ_BOX    p);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint64_t    benz_iso_fullbox_get_size_bounded(BENZ_BOX    p, uint64_t max_size);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE const char* benz_iso_fullbox_get_type_str    (BENZ_BOX    p);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint64_t    benz_iso_fullbox_get_type        (BENZ_BOX    p);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE int         benz_iso_fullbox_is_type         (BENZ_BOX    p, const void* type);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE const char* benz_iso_fullbox_get_exttype     (BENZ_BOX    p);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint32_t    benz_iso_fullbox_get_flagver     (BENZ_BOX    p);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint32_t    benz_iso_fullbox_get_version     (BENZ_BOX    p);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint32_t    benz_iso_fullbox_get_flags       (BENZ_BOX    p);
-BENZINA_PUBLIC BENZINA_EXTERN_INLINE const void* benz_iso_fullbox_get_payload     (BENZ_BOX    p);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint64_t    benz_iso_box_get_size            (const BENZ_ISO_BOX*   p);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint64_t    benz_iso_box_get_size_bounded    (const BENZ_ISO_BOX*   p, uint64_t max_size);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE const char* benz_iso_box_get_type_str        (const BENZ_ISO_BOX*   p);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint32_t    benz_iso_box_get_type            (const BENZ_ISO_BOX*   p);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE int         benz_iso_box_is_type             (const BENZ_ISO_BOX*   p, const void* type);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE const char* benz_iso_box_get_exttype         (const BENZ_ISO_BOX*   p);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE const void* benz_iso_box_get_payload         (const BENZ_ISO_BOX*   p);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint64_t    benz_iso_fullbox_get_size        (const BENZ_ISO_BOX*   p);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint64_t    benz_iso_fullbox_get_size_bounded(const BENZ_ISO_BOX*   p, uint64_t max_size);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE const char* benz_iso_fullbox_get_type_str    (const BENZ_ISO_BOX*   p);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint64_t    benz_iso_fullbox_get_type        (const BENZ_ISO_BOX*   p);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE int         benz_iso_fullbox_is_type         (const BENZ_ISO_BOX*   p, const void* type);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE const char* benz_iso_fullbox_get_exttype     (const BENZ_ISO_BOX*   p);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint32_t    benz_iso_fullbox_get_flagver     (const BENZ_ISO_BOX*   p);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint32_t    benz_iso_fullbox_get_version     (const BENZ_ISO_BOX*   p);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE uint32_t    benz_iso_fullbox_get_flags       (const BENZ_ISO_BOX*   p);
+BENZINA_PUBLIC BENZINA_EXTERN_INLINE const void* benz_iso_fullbox_get_payload     (const BENZ_ISO_BOX*   p);
 
