f42a3586190afbdca4aa0140fb33cea91b78d0f7
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2021-03-22 16:41:47 -0400

    Update pybenzinaparse dependency

--- scripts/setup.py
+++ scripts/setup.py
@@ -77,7 +77,7 @@ setup(
         "meson>=0.54.0",
         "numpy>=1.10",
         "pytest==6.0.1",
-        "pybenzinaparse @ git+https://github.com/satyaog/pybenzinaparse.git@653e71586726ed04293d7362e3539e26dc28850c#egg=pybenzinaparse-0.2.1",
+        "pybenzinaparse @ git+https://github.com/satyaog/pybenzinaparse.git@0.2.2#egg=pybenzinaparse-0.2.2",
     ],
     extras_require={"psnrhma": ["opencv-python",
                                 "scipy"]},
