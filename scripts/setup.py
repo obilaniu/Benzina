876b4d1f06ea7bb2099c64c9b9abc3df130a1099
Olexa Bilaniuk <obilaniu@gmail.com>
2021-06-18 10:27:52 -0400

    Update Meson requirement in setup.py.

--- scripts/setup.py
+++ scripts/setup.py
@@ -71,10 +71,10 @@ setup(
     zip_safe             = False,
     python_requires      = '>=3.6',
     setup_requires       = [
-        "meson>=0.54.0",
+        "meson>=0.58.1",
     ],
     install_requires     = [
-        "meson>=0.54.0",
+        "meson>=0.58.1",
         "numpy>=1.10",
         "pytest==6.0.1",
         "pybenzinaparse @ git+https://github.com/satyaog/pybenzinaparse.git@653e71586726ed04293d7362e3539e26dc28850c#egg=pybenzinaparse-0.2.1",
